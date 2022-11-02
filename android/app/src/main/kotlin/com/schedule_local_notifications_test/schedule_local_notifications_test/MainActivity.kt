5d19911a5ca8e38a7e5d5b5b90b0ff51e77f20a5
Shahbaz Chaudhry <shahbaz21144@gmail.com>
2022-11-02 15:34:52 -0400

    first

---
+++ android/app/src/main/kotlin/com/schedule_local_notifications_test/schedule_local_notifications_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.schedule_local_notifications_test.schedule_local_notifications_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,288 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import 'InfoValueClass.dart';
+import 'PaddedElevatedButton.dart';
+import 'main.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+class HomePage extends StatefulWidget {
+  const HomePage(
+      this.notificationAppLaunchDetails, {
+        Key? key,
+      }) : super(key: key);
+
+  static const String routeName = '/';
+
+  final NotificationAppLaunchDetails? notificationAppLaunchDetails;
+
+  bool get didNotificationLaunchApp =>
+      notificationAppLaunchDetails?.didNotificationLaunchApp ?? false;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  bool _notificationsEnabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _isAndroidPermissionGranted();
+    _requestPermissions();
+    _configureDidReceiveLocalNotificationSubject();
+    _configureSelectNotificationSubject();
+  }
+
+  Future<void> _isAndroidPermissionGranted() async {
+    if (Platform.isAndroid) {
+      final bool granted = await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+          ?.areNotificationsEnabled() ??
+          false;
+
+      setState(() {
+        _notificationsEnabled = granted;
+      });
+    }
+  }
+
+  Future<void> _requestPermissions() async {
+    if (Platform.isIOS || Platform.isMacOS) {
+      await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+          IOSFlutterLocalNotificationsPlugin>()
+          ?.requestPermissions(
+        alert: true,
+        badge: true,
+        sound: true,
+        critical: true,
+      );
+      await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+          MacOSFlutterLocalNotificationsPlugin>()
+          ?.requestPermissions(
+        alert: true,
+        badge: true,
+        sound: true,
+        critical: true,
+      );
+    } else if (Platform.isAndroid) {
+      final AndroidFlutterLocalNotificationsPlugin? androidImplementation =
+      flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>();
+
+      final bool? granted = await androidImplementation?.requestPermission();
+      setState(() {
+        _notificationsEnabled = granted ?? false;
+      });
+    }
+  }
+
+  void _configureDidReceiveLocalNotificationSubject() {
+    didReceiveLocalNotificationStream.stream
+        .listen((ReceivedNotification receivedNotification) async {
+      await showDialog(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+          title: receivedNotification.title != null
+              ? Text(receivedNotification.title!)
+              : null,
+          content: receivedNotification.body != null
+              ? Text(receivedNotification.body!)
+              : null,
+          actions: <Widget>[
+            CupertinoDialogAction(
+              isDefaultAction: true,
+              onPressed: () async {
+                // Navigator.of(context, rootNavigator: true).pop();
+                // await Navigator.of(context).push(
+                //   MaterialPageRoute<void>(
+                //     builder: (BuildContext context) =>
+                //         SecondPage(receivedNotification.payload),
+                //   ),
+                // );
+              },
+              child: const Text('Ok'),
+            )
+          ],
+        ),
+      );
+    });
+  }
+
+  void _configureSelectNotificationSubject() {
+    selectNotificationStream.stream.listen((String? payload) async {
+      // await Navigator.of(context).push(MaterialPageRoute<void>(
+      //   builder: (BuildContext context) => SecondPage(payload),
+      // ));
+    });
+  }
+
+  @override
+  void dispose() {
+    didReceiveLocalNotificationStream.close();
+    selectNotificationStream.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: const Text('Plugin example app'),
+    ),
+    body: SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                child:
+                Text('Tap on a notification when it appears to trigger'
+                    ' navigation'),
+              ),
+              InfoValueString(
+                title: 'Did notification launch app?',
+                value: widget.didNotificationLaunchApp,
+              ),
+              if (widget.didNotificationLaunchApp) ...<Widget>[
+                const Text('Launch notification details'),
+                InfoValueString(
+                    title: 'Notification id',
+                    value: widget.notificationAppLaunchDetails!
+                        .notificationResponse?.id),
+                InfoValueString(
+                    title: 'Action id',
+                    value: widget.notificationAppLaunchDetails!
+                        .notificationResponse?.actionId),
+                InfoValueString(
+                    title: 'Input',
+                    value: widget.notificationAppLaunchDetails!
+                        .notificationResponse?.input),
+                InfoValueString(
+                  title: 'Payload:',
+                  value: widget.notificationAppLaunchDetails!
+                      .notificationResponse?.payload,
+                ),
+              ],
+
+              if (kIsWeb || !Platform.isLinux) ...<Widget>[
+                PaddedElevatedButton(
+                  buttonText: 'Repeat notification every minute',
+                  onPressed: () async {
+                    await _repeatNotification();
+                  },
+                ),
+                PaddedElevatedButton(
+                  buttonText:
+                  'Schedule daily 10:00:00 am notification in your '
+                      'local time zone',
+                  onPressed: () async {
+                    await _scheduleDailyTenAMNotification();
+                  },
+                ),
+                PaddedElevatedButton(
+                  buttonText:
+                  'Schedule daily 10:00:00 am notification in your '
+                      "local time zone using last year's date",
+                  onPressed: () async {
+                    await _scheduleDailyTenAMLastYearNotification();
+                  },
+                ),
+
+              ],
+
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+
+
+
+
+
+  Future<void> _repeatNotification() async {
+    const AndroidNotificationDetails androidNotificationDetails =
+    AndroidNotificationDetails(
+        'repeating channel id', 'repeating channel name',
+        channelDescription: 'repeating description');
+    const NotificationDetails notificationDetails =
+    NotificationDetails(android: androidNotificationDetails);
+    await flutterLocalNotificationsPlugin.periodicallyShow(
+        1,
+        'repeating title',
+        'repeating body',
+        RepeatInterval.everyMinute,
+        notificationDetails,
+        androidAllowWhileIdle: true);
+  }
+
+  Future<void> _scheduleDailyTenAMNotification() async {
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+        2,
+        'daily scheduled notification title',
+        'daily scheduled notification body',
+        _nextInstanceOfTenAM(),
+        const NotificationDetails(
+          android: AndroidNotificationDetails('daily notification channel id',
+              'daily notification channel name',
+              channelDescription: 'daily notification description'),
+        ),
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+        UILocalNotificationDateInterpretation.absoluteTime,
+        matchDateTimeComponents: DateTimeComponents.time);
+  }
+
+  /// To test we don't validate past dates when using `matchDateTimeComponents`
+  Future<void> _scheduleDailyTenAMLastYearNotification() async {
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+        3,
+        'daily scheduled notification title',
+        'daily scheduled notification body',
+        _nextInstanceOfTenAMLastYear(),
+        const NotificationDetails(
+          android: AndroidNotificationDetails('daily notification channel id',
+              'daily notification channel name',
+              channelDescription: 'daily notification description'),
+        ),
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+        UILocalNotificationDateInterpretation.absoluteTime,
+        matchDateTimeComponents: DateTimeComponents.time);
+  }
+
+
+  tz.TZDateTime _nextInstanceOfTenAM() {
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local).add(Duration(minutes: 5));
+    tz.TZDateTime scheduledDate =
+    tz.TZDateTime(tz.local, now.year, now.month, now.day, now.hour,now.minute);
+    if (scheduledDate.isBefore(now)) {
+      scheduledDate = scheduledDate.add(const Duration(days: 0));
+    }
+    return scheduledDate;
+  }
+
+  tz.TZDateTime _nextInstanceOfTenAMLastYear() {
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    return tz.TZDateTime(tz.local, now.year - 1, now.month, now.day, 19,30);
+  }
+
+
+
+
+
+
+}
